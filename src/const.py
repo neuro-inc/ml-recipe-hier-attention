048bf3a5a1fbc5deda398c24c1f825da0e92b50c
Artem Yushkovskiy <ajuszkowski@ya.ru>
2020-04-14 17:09:12 +0300

    Cleanup

--- src/const.py
+++ src/const.py
@@ -4,7 +4,7 @@ from pathlib import Path
 data_path_str = os.environ.get("DATA_PATH_ENV")
 
 ROOT_PATH = Path(__file__).parent.parent
-DATA_PATH = Path(data_path_str) if data_path_str else ROOT_PATH / 'data'
+DATA_PATH = Path(data_path_str or ROOT_PATH / 'data')
 RESULT_DIR = ROOT_PATH / 'results'
 
 VECTORS_CACHE = RESULT_DIR / '.vector_cache'
